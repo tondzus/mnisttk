d8d9d36f482f41377ac77c4faf548659d373fa04
Tomas Stibrany <tms.stibrany@gmail.com>
2015-11-10 15:21:04 +0100

    Added ignored folder for test/train results.

--- train_mnist.py
+++ train_mnist.py
@@ -47,5 +47,5 @@ validator = ml.CrossValidator(ml.ForwardFeedNetwork, (28*28, 100, 10))
 try:
     models = validator.run(data, 5, 0.4, ml.max_epochs(100))
 finally:
-    with open('mnist.ann', 'wb') as fp:
+    with open('results/mnist.ann', 'wb') as fp:
         pickle.dump(models, fp)
